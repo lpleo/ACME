54af6ca4125b1c987178d03efb5c022ec8c3de43
lpleo <lpleo@hotmail.it>
2018-07-06 15:00:08 +0200

    Added application.yml and h2 support for local development

--- src/test/java/it/lpleo/management/camp/ApplicationRunnerDevelopment.java
+++ src/test/java/it/lpleo/management/camp/ApplicationRunnerDevelopment.java
@@ -1,3 +1,13 @@
 package it.lpleo.management.camp;
 
-public class ApplicationRunnerDevelopment {}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunnerDevelopment {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.profiles.active", "development");
+		SpringApplication.run(Application.class, args);
+	}
+}
