05667a1735637eac84bf2429ab995d89e1de3681
lpleo <lpleo@hotmail.it>
2018-07-09 17:22:19 +0200

    Updated database schema and added more queries

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -1,1 +1,12 @@
 INSERT INTO CAMP(name,`year`,active) VALUES ('TESTCAMPACTIVE',2008,1);
+
+INSERT INTO CHILD(fiscalCode, name, surname, birthDate)
+VALUES ('AAABBB55G50W548A','CIAO','PIPPO', NOW());
+
+INSERT INTO PARENT(fiscalCode, name, surname, email, telephoneNumber)
+VALUES ('AAABBB55G50W548A','LOLLO','LOLLI', 'abc@ciao.it','123456789');
+
+INSERT INTO ALLERGY(childId,name,description,type)
+VALUES (1,'BREAD','Chocolate flavor bread', 'FOOD');
+INSERT INTO ALLERGY(childId,name,description,type)
+VALUES (1,'PASTA','Chocolate flavor pasta', 'FOOD');
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -17,38 +17,43 @@ CREATE TABLE PARENT (
 );
 
 
-CREATE TABLE CHILDREN (
+CREATE TABLE CHILD (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    parentId MEDIUMINT NOT NULL,
     fiscalCode varchar(255),
     name varchar(255),
     surname varchar(255),
     birthDate date,
-    weekNumber int,
-    PRIMARY KEY(id),
-    FOREIGN KEY(parentId) REFERENCES PARENT(id)
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE PARENT_CHILD (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    parentId MEDIUMINT NOT NULL,
+    childId MEDIUMINT NOT NULL,
+    FOREIGN KEY(parentId) REFERENCES PARENT(id),
+    FOREIGN KEY(childId) REFERENCES CHILD(id)
 );
 
 CREATE TABLE SUBSCRIPTION (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
     campId MEDIUMINT NOT NULL,
-    childrenId MEDIUMINT NOT NULL,
+    childId MEDIUMINT NOT NULL,
     month int,
     firstDay int,
     lastDay int,
     weekNumber int,
     PRIMARY KEY(id),
     FOREIGN KEY(campId) REFERENCES CAMP(id),
-    FOREIGN KEY(childrenId) REFERENCES CHILDREN(id)
+    FOREIGN KEY(childId) REFERENCES CHILD(id)
 );
 
 CREATE TABLE ALLERGY(
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    childrenId MEDIUMINT NOT NULL,
+    childId MEDIUMINT NOT NULL,
     name varchar(255),
     description varchar(255),
     type varchar(255),
     PRIMARY KEY(id),
-    FOREIGN KEY(childrenId) REFERENCES CHILDREN(id)
+    FOREIGN KEY(childId) REFERENCES CHILD(id)
 );
 
